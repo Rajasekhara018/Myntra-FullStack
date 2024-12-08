88d9975f005f9afd0663ad9722812d19c110aed9
Rajasekhara018 <rajshekarbadam001@gmail.com>
2024-12-08 12:12:50 +0530

    Myntra Backend changes

--- Myntra-Backend/src/main/java/com/myntra/repository/MailResponseReposirory.java
+++ Myntra-Backend/src/main/java/com/myntra/repository/MailResponseReposirory.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MailResponseReposirory extends MongoRepository<EmailResponse, String> {
+public interface MailResponseReposirory extends MongoRepository<EmailResponse, Long> {
 }
